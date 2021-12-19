72c9233ec2382117946dd703f9c3d17c5e26b99d
Kotynia <piotr.kotynia@gmail.com>
2021-12-19 10:53:27 +0100

    init commit

---
+++ src/main/kotlin/com/pocodex/calendar/CalendarApplication.kt
@@ -0,0 +1,11 @@
+package com.pocodex.calendar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalendarApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalendarApplication>(*args)
+}
