1865fb70eb072348f60d80727efc220e0a1b04b3
Domek <domek@MacBook-Air-Domek.local>
2022-01-23 12:12:47 +0100

    dockerfile + docker compose

--- README.md
+++ README.md
@@ -4,12 +4,6 @@ few useful commands:
 
 # setup for docker postgers
 
-docker run -d \
---name dev-postgres \
--e POSTGRES_PASSWORD=admin \
--p 54320:5432 \
-postgres
-
 docker exec -it dev-postgres psql -U postgres
 
 curls for testing:
--- src/main/kotlin/com/polodex/calendar/UserWebClient.kt
+++ src/main/kotlin/com/polodex/calendar/UserWebClient.kt
@@ -4,14 +4,14 @@ import org.springframework.web.reactive.function.client.WebClient
 
 val client = WebClient.create("http://localhost:8080")
 
-fun main(args: Array<String>) {
-    client.get().uri("calendars?id=1")
-        .retrieve()
-        .bodyToFlux(Calendar::class.java)
-        .subscribe { println("find me $it") }
-
-    //welcome to nonblocking world :)
-    Thread.sleep(5000)
-
-    println("finished")
-}
+//fun main(args: Array<String>) {
+//    client.get().uri("calendars?id=1")
+//        .retrieve()
+//        .bodyToFlux(Calendar::class.java)
+//        .subscribe { println("find me $it") }
+//
+//    //welcome to nonblocking world :)
+//    Thread.sleep(5000)
+//
+//    println("finished")
+//}
